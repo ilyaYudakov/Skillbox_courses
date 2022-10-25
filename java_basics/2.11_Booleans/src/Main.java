de9b831bb0822fbb39d601017c4b57011dc04e36
ilya <ilyuno@ya.ru>
2022-10-25 21:58:42 +0300

    lesson 2

--- java_basics/2.11_Booleans/src/Main.java
+++ java_basics/2.11_Booleans/src/Main.java
@@ -1,10 +1,10 @@
 
 public class Main {
     public static void main(String[] args) {
-        int milkAmount = 200; // ml
-        int powderAmount = 5; // g
+        int milkAmount = 1000; // ml
+        int powderAmount = 400; // g
         int eggsCount = 3; // items
-        int sugarAmount = 5; // g
+        int sugarAmount = 10; // g
         int oilAmount = 30; // ml
         int appleCount = 8; // items
 
@@ -15,12 +15,21 @@ public class Main {
         }
 
         // powder - 400 g, sugar - 10 g, milk - 1 l, oil - 30 ml
-        System.out.println("Pancakes");
+        if (powderAmount >= 400 && sugarAmount >= 10
+                && milkAmount >= 1000 && oilAmount >= 30) {
+            System.out.println("Pancakes");
+        }
 
         // milk - 300 ml, powder - 5 g, eggs - 5
-        System.out.println("Omelette");
+        if (powderAmount >= 5
+                && milkAmount >= 300 && eggsCount >= 5) {
+            System.out.println("Omelette");
+        }
 
         // apples - 3, milk - 100 ml, powder - 300 g, eggs - 4
-        System.out.println("Apple pie");
+        if (appleCount >= 3 && milkAmount >= 100
+                && powderAmount >=300 && eggsCount >=4) {
+            System.out.println("Apple pie");
+        }
     }
 }
